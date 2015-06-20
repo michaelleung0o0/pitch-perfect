33fe02085ad7f00cd765aa8ca987c15ad34fb755
michaelleung <michael.leung@mbounce.com>
2015-06-20 22:21:57 +0800

    Initial Commit

---
+++ pitch perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pitch perfect
+//
+//  Created by MichaelLeung on 20/6/15.
+//  Copyright (c) 2015 MichaelLeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
